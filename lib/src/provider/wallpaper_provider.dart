d0ad0214233c8432252f53fa9d1a5baebd136f8c
mayman007 <medoems2210@gmail.com>
2024-11-17 03:13:58 +0200

    changed how getting wall info works

--- lib/src/provider/wallpaper_provider.dart
+++ lib/src/provider/wallpaper_provider.dart
@@ -9,14 +9,22 @@ class WallpaperProvider extends ChangeNotifier {
   final String baseUrl = '${dotenv.env['API_URL']}/wallpapers';
 
   List<String> _wallpaperNames = [];
+  List<String> _wallpaperResolutions = [];
+  List<int> _wallpaperSizes = [];
+  List<String> _wallpaperCategories = [];
+  List<dynamic> _wallpaperColors = [];
   bool _isLoading = false;
   bool _isError = false;
 
   List<String> get wallpaperNames => _wallpaperNames;
+  List<String> get wallpaperResolutions => _wallpaperResolutions;
+  List<int> get wallpaperSizes => _wallpaperSizes;
+  List<String> get wallpaperCategories => _wallpaperCategories;
+  List<dynamic> get wallpaperColors => _wallpaperColors;
   bool get isLoading => _isLoading;
   bool get isError => _isError;
 
-  Future<void> fetchWallpaperNames() async {
+  Future<void> fetchWallpaperData() async {
     _isLoading = true;
     _isError = false;
     notifyListeners();
@@ -24,13 +32,27 @@ class WallpaperProvider extends ChangeNotifier {
     try {
       final response = await _dio.get(baseUrl);
       if (response.statusCode == 200 && response.data is List) {
-        _wallpaperNames = List<String>.from(response.data);
+        for (Map wallpaper in response.data) {
+          _wallpaperNames.add(wallpaper["name"]);
+          _wallpaperResolutions.add(wallpaper["resolution"]);
+          _wallpaperSizes.add(wallpaper["size"]);
+          _wallpaperCategories.add(wallpaper["category"]);
+          _wallpaperColors.add(wallpaper["colors"]);
+        }
       } else {
         _wallpaperNames = [];
+        _wallpaperResolutions = [];
+        _wallpaperSizes = [];
+        _wallpaperCategories = [];
+        _wallpaperColors = [];
       }
     } catch (e) {
       log('Error fetching wallpaper names: $e');
       _wallpaperNames = [];
+      _wallpaperResolutions = [];
+      _wallpaperSizes = [];
+      _wallpaperCategories = [];
+      _wallpaperColors = [];
       _isError = true;
     } finally {
       log("Wallpaper fetching done.");
