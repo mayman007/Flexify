60ac0b4537aff0885c085487e61595f34525d137
Mohamed Ayman <medoems2210@gmail.com>
2025-01-21 16:43:07 +0200

    updated comments

--- lib/main.dart
+++ lib/main.dart
@@ -11,22 +11,16 @@ import 'src/provider/wallpaper_category_provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  // Init analytics
   await AnalyticsEngine.init();
   AnalyticsEngine.appOpened;
-  // Set up the SettingsController, which will glue user settings to multiple
-  // Flutter Widgets.
 
-  // Load the user's preferred theme while the splash screen is displayed.
-  // This prevents a sudden theme change when the app is first displayed.
-
-  // Run the app and pass in the SettingsController. The app listens to the
-  // SettingsController for changes, then passes it further down to the
-  // SettingsView.
-
+  // Init db
   DatabaseHelper sqlDb = DatabaseHelper();
   await sqlDb.initialDb();
 
   runApp(
+    // Init provider
     MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => WallpaperProvider()),
