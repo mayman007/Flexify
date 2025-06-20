e88d544ff9da1fe9ac8d0f03eeadac389bcc8b5a
mayman007 <medoems2210@gmail.com>
2025-06-20 15:54:50 +0300

    added firebase notifications

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flexify/src/analytics_engine.dart';
 import 'package:flexify/src/database/database_helper.dart';
 import 'package:flexify/src/provider/depthwall_provider.dart';
@@ -6,14 +7,25 @@ import 'package:flexify/src/provider/widget_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'src/app.dart';
+import 'src/notifications/notifications.dart';
 import 'src/provider/wallpaper_provider.dart';
 import 'src/provider/wallpaper_category_provider.dart';
+
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) =>
+    _firebaseMessagingBackgroundHandler(message);
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   // Init analytics
   await AnalyticsEngine.init();
   AnalyticsEngine.appOpened;
+
+  // Register background handler early
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+  // Initialize your notification service
+  await NotificationService.instance.initialize();
 
   // Init db
   DatabaseHelper sqlDb = DatabaseHelper();
